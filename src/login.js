dfcb7a90b525bc669653df407712ce5107486e02
Natdanai <s6304062620070@email.kmutnb.ac.th>
2023-11-04 00:12:23 +0700

    nuttttttttttttttttttttt

--- src/login.js
+++ src/login.js
@@ -74,7 +74,7 @@ export default function Login() {
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{ mt: 3, mb: 2, backgroundColor: '#00C300', color: '#fff' }}
             >
               เข้าสู่ระบบ
             </Button>
